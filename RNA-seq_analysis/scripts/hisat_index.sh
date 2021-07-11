5cfd8c402783d3a438263e98fd2d142dfab37cd9
pjmartinez <pjmartinez@cebas.csic.es>
2021-07-11 22:10:37 +0200

    Create hisat_index.sh

---
+++ RNA-seq_analysis/scripts/hisat_index.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+#SBATCH -J hisat_index
+#SBATCH -p generic       # Partición (cola)
+##SBATCH -N 1                # Numero de nodos
+##SBATCH -n 16                 # Numero de cores(CPUs)
+#SBATCH -w trueno186
+#SBATCH --mem-per-cpu=5000    # Bloque de memoria para todos los nodos
+#SBATCH -t 5-02:00     # Duración (D-HH:MM)
+#SBATCH --output=/home/cebas/pmartinez/secuencias/TFM_vitis/temp/slurm-%j.out  #STDOUT
+#SBATCH --error=/home/cebas/pmartinez/secuencias/TFM_vitis/temp/slurm-%j.err   #STDERR
+#SBATCH --mail-type=END,FAIL      # Notificación cuando el trabajo termina o falla
+#SBATCH --mail-user=pjmartinez@cebas.csic.es # Enviar correo a la dirección
+
+#export _JAVA_OPTIONS=-Djava.io.tmpdir=/scratch
+
+
+/home/cebas/pmartinez/hisat2-2.1.0/hisat2-build -p 16 /home/cebas/pmartinez/secuencias/TFM_vitis/PinorNoir_genome/all_chromosomes/PinotNoir.fa /home/cebas/pmartinez/secuencias/TFM_vitis/PinorNoir_genome/hisat_index/pinotnoir
+
+
+
