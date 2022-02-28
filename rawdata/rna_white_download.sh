e8bc4fe5176e24683d5274ae808e75c7537e5646
pjmartinez <pjmartinez@cebas.csic.es>
2022-02-28 12:11:38 +0100

    Update rna_white_download.sh

--- rawdata/rna_white_download.sh
+++ rawdata/rna_white_download.sh
@@ -1,4 +1,16 @@
 
+#!/bin/bash
+#SBATCH --job-name=JOBNAME #Gives a user specified name to the job.
+#SBATCH -n 1 #Task count
+#SBATCH -N 1 #Node count
+#SBATCH -c 1 #CPUs/cores per task
+#SBATCH --mem=1G #job memory request per node, usually an integer followed by a prefix for the unit (e. g. --mem=1G for 1 GB)
+#SBATCH --partition=general # Run the job in the specified partition/queue depend of your server.
+#SBATCH --qos= general #Defines the quality-of-service to be used for the job.
+#SBATCH --mail-type=ALL #Defines when a mail message about the job will be sent to the user. See the man page for details.
+#SBATCH --mail-user=youremail
+#SBATCH -o %x_%j.out #Specifies the file name to be used for stdout.
+#SBATCH -e %x_%j.err #Specifies the file name to be used for stderr.
 
 
 
