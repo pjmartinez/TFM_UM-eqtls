f6221d3a92076ebdc645299b21f95d09e04ba7bb
pjmartinez <pjmartinez@cebas.csic.es>
2022-03-01 10:10:12 +0100

    Update aligh.sh

--- RNA-seq_analysis/align/aligh.sh
+++ RNA-seq_analysis/align/aligh.sh
@@ -1,1 +1,15 @@
 
+
+
+
+
+
+
+for file in *_trim.fastq.gz
+do name=$(basename $file _trim.fastq.gz) 
+
+hisat2 -p 8 --dta -x /home/cebas/pmartinez/secuencias/TFM_vitis/PinorNoir_genome/hisat_index/pinotnoir -U ${name}_trim.fastq.gz | \
+        samtools view -S -h -u - | \
+        samtools sort -T ${name} - > ./"$dir"/${name}.bam
+
+echo "===================hisat align started at `date` for ============" $name
